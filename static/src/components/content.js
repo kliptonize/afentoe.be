480e0513bb79d2dec7f5f2a75f1bd343d23778a4
Koen <koen@beformed.be>
2018-08-06 22:06:58 +0200

    Added actionable fragments
    - eg: up the number of times something happened

--- static/src/components/content.js
+++ static/src/components/content.js
@@ -1,10 +1,57 @@
 import React from 'react';
+ 
+import NumberAction from './fragments/number.action';
+import TextAction from './fragments/text.action';
+import DateAction from './fragments/date.action';
 
 class Content extends React.Component {
+ constructor(){
+    super();
+
+    this.state = {
+      subject: "Michiel",
+      verb: "vloekt",
+      description: "Hoe vaak heeft Michiel ondertussen al gevloekt?",
+      type: "number",
+      data: "1204"
+    }
+  }
+
+  update(data){
+  	console.log(data);
+  }
+
   render() {
-    return (
+  	var action = null;
+  	switch(this.state.type){
+  		case "number":
+  			action = <NumberAction callback={this.update}/>
+  			break;
+  		case "text":
+  			action = <TextAction callback={this.update}/>
+  			break;
+  		case "date":
+  			action = <DateAction callback={this.update}/>
+  			break;
+  		default:
+  			action = "<br/>";
+  			break;
+  	}
+
+    return ( 
       <section>
-        It's some content!
+        <div className="box box--center box--white">
+          <section className="text--center">
+            <h1>{this.state.subject} {this.state.verb}</h1>
+          </section>
+          <section className="text--center">
+          	<p>{this.state.description}</p>
+          	<p>{this.state.data}</p>
+          </section>
+          <section>
+          	{action}
+          </section>
+         </div>
       </section>
     );
   };
